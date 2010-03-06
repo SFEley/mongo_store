7ec8f136f3553917d068fa894c032413c6c4dffc
Stephen Eley <sfeley@gmail.com>
2010-03-06 18:25:17 -0500

    Rewrote initialization and made options more flexible. Added #clean_expired and #clear methods.

--- lib/active_support/cache/mongo_store.rb
+++ lib/active_support/cache/mongo_store.rb
@@ -4,27 +4,63 @@ require 'mongo'
 module ActiveSupport
   module Cache
     class MongoStore < Store
-      attr_reader :collection
+      attr_reader :options
+      attr_accessor :expires_in
       
       # Returns a MongoDB cache store.  Can take either a Mongo::Collection object or a collection name.
-      # If neither is provided, a collection named "rails_store" is created.
+      # If neither is provided, a collection named "rails_cache" is created.
       #
       # An options hash may also be provided with the following options:
       # 
       # * :expires_in - The default expiration period for cached objects. If not provided, defaults to 1 year.
-      # * :db - Either a Mongo::DB object or a database name. Not used if a Mongo::Collection object is passed. Otherwise defaults to MongoMapper.database (if MongoMapper is used in the app) or else creates a DB named 'rails_cache'.
+      # * :db - Either a Mongo::DB object or a database name. Not used if a Mongo::Collection object is passed. Otherwise defaults to MongoMapper.database (if MongoMapper is used in the app) or else creates a DB named "rails_cache".
       # * :create_index - Whether to index the key and expiration date on the collection. Defaults to true. Not used if a Mongo::Collection object is passed.
-      def initialize(collection = nil, db_name = nil)
-        @collection = case collection
-        when Mongo::Collection then collection
-        
+      def initialize(collection = nil, options = nil)
+        @options = {
+          :collection_name => 'rails_cache',
+          :db_name => 'rails_cache',
+          :expires_in => 1.year,
+          :create_index => true
+        }
+        # @options.merge!(options) if options
+        case collection
+        when Mongo::Collection
+          @collection = collection
         when String
-          make_collection(collection, db_name)
+          @options[:collection_name] = collection
+        when Hash
+          @options.merge!(collection)
         when nil
-          make_collection('rails_cache')
+          # No op
         else
-          raise TypeError, "MongoStore parameters must be nil, a Mongo::Collection, or a collection name."
+          raise TypeError, "MongoStore parameters must be a Mongo::Collection, a collection name, and/or an options hash."
         end
+        
+        @options.merge!(options) if options.is_a?(Hash)
+        
+        # Set the expiration time
+        self.expires_in = @options[:expires_in]
+        super()
+      end
+      
+      # Returns the MongoDB collection described in the options to .new (or else the default 'rails_cache' one.)
+      # Lazily creates the object on first access so that we can look for a MongoMapper database _after_ 
+      # MongoMapper initializes.
+      def collection
+        @collection ||= make_collection
+      end
+            
+      # Removes old cached values that have expired.  Set this up to run occasionally in delayed_job, etc., if you
+      # start worrying about space.  (In practice, because we favor updating over inserting, space is only wasted
+      # if the key itself never gets cached again.  It also means you can _reduce_ efficiency by running this
+      # too often.)
+      def clean_expired
+        collection.remove({'expires' => {'$lt' => Time.now}})
+      end
+      
+      # Wipes the whole cache.
+      def clear
+        collection.remove
       end
       
       # Inserts the value into the cache collection or updates the existing value.  The value must be a valid
@@ -32,7 +68,7 @@ module ActiveSupport
       # provided, a default expiration of 1 year is used.
       def write(key, value, options=nil)
         super
-        expires = Time.now + ((options && options[:expires_in]) || 1.year)
+        expires = Time.now + ((options && options[:expires_in]) || expires_in)
         collection.update({'key' => key}, {'$set' => {'value' => value, 'expires' => expires}}, :upsert => true)
       end
       
@@ -58,16 +94,19 @@ module ActiveSupport
         Kernel.const_defined?(:MongoMapper) && MongoMapper.respond_to?(:database) && MongoMapper.database
       end
       
-      def make_collection(collection, db_name=nil)
-         if db_name
-           db = Mongo::DB.new(db_name, Mongo::Connection.new)
-         elsif mongomapper?
-           db = MongoMapper.database
-         else
-           db = Mongo::DB.new(collection, Mongo::Connection.new)
-         end
-        coll = db.create_collection(collection)
-        coll.create_index('key' => Mongo::ASCENDING, 'expires' => Mongo::DESCENDING)
+      def make_collection
+        db = case options[:db]
+        when Mongo::DB then options[:db]
+        when String then Mongo::DB.new(options[:db], Mongo::Connection.new)
+        else
+          if mongomapper?
+            MongoMapper.database
+          else
+            Mongo::DB.new(options[:db_name], Mongo::Connection.new)
+          end
+        end
+        coll = db.create_collection(options[:collection_name])
+        coll.create_index('key' => Mongo::ASCENDING, 'expires' => Mongo::DESCENDING) if options[:create_index]
         coll
       end
         
